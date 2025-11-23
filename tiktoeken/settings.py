5dece7172032c2cb14ff5ad5ce89b717d7f89d3a
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-11-23 17:45:00 +0500

    feat: добавлена поддержка переменных окружения для конфигурации

--- tiktoeken/settings.py
+++ tiktoeken/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7fzzb2efnu)hnjf(_j&gm^6@(@lg0j=vo!f76+a-iyw@mr0o@8'
+SECRET_KEY = os.getenv('SECRET_KEY', 'your-default-secret-key-here')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG_MODE', True) == 'True'
+
 
 ALLOWED_HOSTS = []
 
